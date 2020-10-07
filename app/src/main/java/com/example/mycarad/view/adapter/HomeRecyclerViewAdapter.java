350ff7dbf41978604d40cf73f36ed0e3f3ff1a9e
zinc <zinc0214@gmail.com>
2020-10-08 03:46:08 +0900

    HomeRecyclerViewAdapter 에서 onClick에 대한 기능

--- app/src/main/java/com/example/mycarad/view/adapter/HomeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/mycarad/view/adapter/HomeRecyclerViewAdapter.java
@@ -1,16 +1,19 @@
 package com.example.mycarad.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mycarad.R;
 import com.example.mycarad.data.HomeData;
+import com.example.mycarad.view.activity.HomeActivity;
 
 import java.util.ArrayList;
 
@@ -56,6 +59,17 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
 
             TextView tv2 = itemView.findViewById(R.id.timeTextView);
             tv2.setText(home.getTime());
+
+            tv1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, "눌렸다!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(context, HomeActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+
         }
 
 
