1885e111d333561be1b298439160e601fcd5c6f7
zinc <zinc0214@gmail.com>
2020-10-08 03:47:21 +0900

    getApplicationContext -> this 를 사용하는 걸로 수정

--- app/src/main/java/com/example/mycarad/view/activity/LoginActivity.java
+++ app/src/main/java/com/example/mycarad/view/activity/LoginActivity.java
@@ -4,18 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mycarad.R;
-
-import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -75,10 +70,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         if (v == loginBtn) {
-            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
         } else if (v == signupBtn) {
-            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            Intent intent = new Intent(this, SignupActivity.class);
             startActivity(intent);
         }
     }
