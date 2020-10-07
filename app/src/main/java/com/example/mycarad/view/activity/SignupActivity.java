af5d9108811f2333dc8092cd0081cf2c41eab098
snow <qlsdl16@gmail.com>
2020-10-08 02:19:31 +0900

    빡통대가리의 스피너

--- app/src/main/java/com/example/mycarad/view/activity/SignupActivity.java
+++ app/src/main/java/com/example/mycarad/view/activity/SignupActivity.java
@@ -2,7 +2,11 @@ package com.example.mycarad.view.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -10,9 +14,15 @@ import androidx.databinding.DataBindingUtil;
 import com.example.mycarad.R;
 import com.example.mycarad.databinding.ActivitySignupBinding;
 
+import java.util.ArrayList;
+
 public class SignupActivity extends AppCompatActivity {
 
     private ActivitySignupBinding binding;
+
+    private Spinner carKind;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +41,31 @@ public class SignupActivity extends AppCompatActivity {
                 }
             }
         });
+
+        arrayList = new ArrayList<>();
+        arrayList.add("대형");
+        arrayList.add("중형");
+        arrayList.add("소형");
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                arrayList);
+
+        carKind = binding.carUserLayout.signCarKindSpinner;
+        carKind.setAdapter(arrayAdapter);
+        carKind.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
     }
+
 
   /*  @Override
     public void onClick(View v){
