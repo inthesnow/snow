a5d85aa69333406b918231e0abe92a20fcbb3686
snow <qlsdl16@gmail.com>
2020-10-22 00:51:30 +0900

    드라이버 뷰 에딧텍스트 -> 텍스트뷰 뷰리스트 리스폰즈 추가

---
+++ app/src/main/java/com/example/mycarad/data/DriverViewListResponse.java
@@ -0,0 +1,17 @@
+package com.example.mycarad.data;
+
+class DriverViewListResponse {
+    private Boolean success;
+
+    public Boolean getSuccesss() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public DriverViewListResponse(Boolean success) {
+        this.success = success;
+    }
+}
